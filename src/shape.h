578acdd537d4ba33e235e331644de732da9bd335
Ryan-O-Connor <74995807+Ryan-O-Connor@users.noreply.github.com>
2021-06-24 06:36:59 -0500

    Moved functions

--- src/shape.h
+++ src/shape.h
@@ -1,2 +1,5 @@
-struct matrix* get_NDERNAT(int lib_id, struct point* pt);
-struct matrix* get_NDERGLB(struct matrix* COORDS, struct matrix* NDERNAT);
+double jacobian(struct matrix* COORDS, struct matrix* NDERNAT);
+struct matrix* construct_COORDS(struct list* nodes, int IEN[], int nenodes);
+struct list* construct_NDERNATs(struct et_def* et);
+struct list* construct_NDERGLBs(struct matrix* COORDS,
+				struct list* NDERNATs, int nint_pts);
